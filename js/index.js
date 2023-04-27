be22c3bdb9eed3c93bd064b6e6436fb6cfcc2481
DESKTOP-B97VBFK\joaqu <leonel.carro94@gmail.com>
2023-04-27 12:26:03 -0300

    Titulo de la navegacion, en mobile desactivado

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 import scrollFunctionNavbar from './navbar.js';
 
 document.addEventListener('DOMContentLoaded', function () {
+	const width = window.matchMedia('(max-width: 700px)');
 	window.onscroll = function () {
-		scrollFunctionNavbar();
+		scrollFunctionNavbar(width);
 	};
 });
--- js/navbar.js
+++ js/navbar.js
@@ -3,11 +3,12 @@ const navbarTitle = document.querySelector('#not-scroll h1');
 const navbarSubtitle = document.getElementById('navbar-subtitle');
 const publish = document.getElementById('publish');
 
-export default function scrollFunctionNavbar() {
+export default function scrollFunctionNavbar(width) {
 	if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
 		//* In scroll */
 		//* Flexbox */
 		navbarSubtitle.style.display = 'none';
+		navbarTitle.style.display = 'block';
 		navbar.style.backgroundColor = 'transparent';
 		navbar.style.display = 'flex';
 		navbar.style.flexDirection = 'row';
@@ -20,11 +21,20 @@ export default function scrollFunctionNavbar() {
 		navbarTitle.style.fontSize = '3rem';
 	} else {
 		//* Out scroll */
+		navbarTitle.style.display = 'block';
 		navbar.style.padding = '';
 		navbar.style.backgroundColor = '#212529';
 		navbarSubtitle.style.display = 'block';
 		navbar.style.flexDirection = 'column';
 		navbarTitle.style.color = '#e4b200';
 		publish.style.display = 'none';
+	}
+
+	if (width.matches) {
+		if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+			navbarTitle.style.display = 'none';
+		}
+	} else {
+		navbarTitle.style.display = 'block';
 	}
 }
