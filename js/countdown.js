0acea3fd0444118124490257f2d9fcbdd3df5c06
DESKTOP-B97VBFK\joaqu <leonel.carro94@gmail.com>
2023-04-27 17:14:26 -0300

    Seccion soft release, countdown terminado.

---
+++ js/countdown.js
@@ -0,0 +1,32 @@
+function countdown() {
+	const targetDate = new Date('May 15, 2023 12:05:02').getTime();
+	const currentDate = new Date().getTime();
+	const difference = targetDate - currentDate;
+
+	const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+	const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+	document.getElementById('days').innerHTML = `
+      <p class="title">${days}</p>
+      <p class="subtitle">Dias</p>
+    `;
+	document.getElementById('hour').innerHTML = `
+      <p class="title text-center">${hours}</p>
+      <span class="title two-points d-none d-lg-block">:</span>
+	  <span class="subtitle d-block d-lg-none">Horas</span>
+    `;
+	document.getElementById('minutes').innerHTML = `
+      <p class="text-center title">${minutes}</p>
+      <span class="title two-points d-none d-lg-block">:</span>
+	  <span class="subtitle d-block d-lg-none">Minutos</span>
+    `;
+	document.getElementById('second').innerHTML = `
+      <p class="text-center title">${seconds}</p>
+      <span class="two-points d-none d-lg-block"></span>
+	  <span class="subtitle d-block d-lg-none">Segundos</span>
+    `;
+}
+
+setInterval(countdown, 1000);
