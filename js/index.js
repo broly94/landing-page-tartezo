8c24c0584b4a28518f7ec7b3b3509422297611ca
DESKTOP-B97VBFK\joaqu <leonel.carro94@gmail.com>
2023-04-30 13:47:49 -0300

    Refactorizacion de la navegacion

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 import scrollFunctionNavbar from './navbar.js';
 
 document.addEventListener('DOMContentLoaded', function () {
-	const width = window.matchMedia('(max-width: 700px)');
 	window.onscroll = function () {
-		scrollFunctionNavbar(width);
+		scrollFunctionNavbar();
 	};
 });
--- js/navbar.js
+++ js/navbar.js
@@ -1,38 +1,49 @@
-const navbar = document.getElementById('navbar');
-const navbarTitle = document.querySelector('#not-scroll h1');
-const navbarSubtitle = document.getElementById('navbar-subtitle');
-const publish = document.getElementById('btn-publish');
+const navbar = document.querySelector('.navbar');
+const navbarTitle = document.querySelector('.not-scroll h1');
+const navbarSubtitle = document.querySelector('.navbar-subtitle');
+const publish = document.querySelector('.btn-publish');
+const containerScroll = document.querySelector('.scroll');
 
-export default function scrollFunctionNavbar(width) {
+export default function scrollFunctionNavbar() {
 	if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
 		//* In scroll */
-		//* Flexbox */
-		navbarSubtitle.style.display = 'none';
-		navbarTitle.style.display = 'block';
-		navbar.style.backgroundColor = 'transparent';
-		navbar.style.display = 'flex';
-		navbar.style.flexDirection = 'row';
-		navbar.style.justifyContent = 'space-between';
-		publish.style.display = 'flex';
-		//* Flexbox end */
-		navbar.style.paddingLeft = '2rem';
-		navbar.style.paddingRight = '2rem';
-		navbarTitle.style.color = '#000';
+		//* NAVBAR */
+		containerScroll.style.display = 'block';
+		containerScroll.style.paddingLeft = '20px';
+		navbar.classList.add('navbar-scroll');
+		navbar.classList.remove('navbar-not-scroll');
+
+		//* NAVBAR TITLE */
 		navbarTitle.style.fontSize = '3rem';
-		publish.style.backgroundColor = 'transparent';
-		publish.style.color = 'transparent';
-		publish.style.outline = '1px solid #000';
-		publish.style.borderRadius = '50%';
+		navbarTitle.style.color = '#000';
+		navbarTitle.classList.add('navbar-scroll-title');
+		navbarTitle.classList.remove('navbar-not-scroll-title');
+
+		//* NAVBAR SUBTITLE */
+		navbarSubtitle.classList.add('navbar-scroll-subtitle');
+		navbarSubtitle.classList.remove('navbar-not-scroll-subtitle');
+
+		//* NAVBAR BUTTON PUBLISH */
+		publish.classList.add('navbar-scroll-btn-publish');
+		publish.classList.remove('navbar-not-scroll-btn-publish');
 	} else {
 		//* Out scroll */
-		navbarTitle.style.display = 'block';
-		navbar.style.padding = '';
-		navbar.style.backgroundColor = '#212529';
-		navbarSubtitle.style.display = 'block';
-		navbar.style.flexDirection = 'column';
+		//* NAVBAR */
+		navbar.classList.remove('navbar-scroll');
+		navbar.classList.add('navbar-not-scroll');
+
+		//* NAVBAR TITLE */
+		navbarTitle.style.fontSize = '4rem';
 		navbarTitle.style.color = '#e4b200';
-		publish.style.display = 'none';
-		publish.style.backgroundColor = '#212529';
-		publish.style.color = '#212529';
+		navbarTitle.classList.add('navbar-not-scroll-title');
+		navbarTitle.classList.remove('navbar-scroll-title');
+
+		//* NAVBAR SUBTITLE */
+		navbarSubtitle.classList.remove('navbar-scroll-subtitle');
+		navbarSubtitle.classList.add('navbar-not-scroll-subtitle');
+
+		//* NAVBAR BUTTON PUBLISH */
+		publish.classList.remove('navbar-scroll-btn-publish');
+		publish.classList.add('navbar-not-scroll-btn-publish');
 	}
 }
